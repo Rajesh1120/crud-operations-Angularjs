753681971fb9a17e0adf9406618b58a97d21381e
Rajesh1120 <nrajesh1120@gmail.com>
2024-10-25 20:08:34 -0500

    disable submit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StudentFormComponent } from './student-form/student-form.component';
-import { StudentDataComponent } from './student-data/student-data.component';
-import { CommonModule } from '@angular/common';
+
 
 
 @Component({
