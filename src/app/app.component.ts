418339381f379e0187aed6b12b81efe9c1ff96e2
Rajesh1120 <nrajesh1120@gmail.com>
2024-10-28 17:09:57 -0500

    routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StudentFormComponent } from './student-form/student-form.component';
+import { HomePageComponent } from "./home-page/home-page.component";
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StudentFormComponent],
+  imports: [RouterOutlet, StudentFormComponent, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { StudentDataComponent } from './student-data/student-data.component';
+import { StudentFormComponent } from './student-form/student-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // {
+    //     path:"student-form",
+    //     component: StudentFormComponent
+    // },
+    // {
+    //     path:'',
+    //     component:StudentDataComponent
+    // }
+];
